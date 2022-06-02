95b5719c577ad2620fb3def331fec5587d279efb
Royalboe <auomidejohnson@gmail.com>
2022-06-02 10:40:45 -0700

    Changes

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+        from calculator_1 import add, sub, mul, div
+
+
+        case '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+        case '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+        case '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+        case '/':
+        print("{} / {} = {}".format(a, b, div(a, b)))
