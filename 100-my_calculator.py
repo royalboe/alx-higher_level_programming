df680f097f0669fe8ed485aba69685d2550f3c64
Royalboe <auomidejohnson@gmail.com>
2022-06-02 10:34:32 -0700

    Calculator

---
+++ 100-my_calculator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+        from calculator_1 import add, sub, mul, div
+
+
+
+
+        case '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+        case '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+        case '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+        case '/':
+        print("{} / {} = {}".format(a, b, div(a, b)))
