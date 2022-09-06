83f13b14d34d91ef9e9b688da0bc8f727b6a2769
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2022-09-06 22:35:36 +0100

    Update 4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,23 +1,30 @@
 #!/usr/bin/node
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
-    if (((w = parseInt(w)) > 0) && ((h = parseInt(h)) > 0)) {
+    if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
     }
   }
+
   print () {
-    console.log(('X'.repeat(this.width) + '\n').repeat(this.height - 1) + 'X'.repeat(this.width));
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
   }
+
   rotate () {
-    const h = this.height;
-    const w = this.width;
-    this.width = h;
-    this.height = w;
+    const x = this.width;
+    this.width = this.height;
+    this.height = x;
   }
+
   double () {
-    this.height *= 2;
     this.width *= 2;
+    this.height *= 2;
   }
-}
-module.exports = Rectangle;
+};
