c137869027034cd44f1bdec132fb8673bccf0080
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2022-09-06 22:29:55 +0100

    Update 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if ((w = parseInt(w)) > 0 && (h = parseInt(h)) > 0) {
       this.width = w;
       this.height = h;
