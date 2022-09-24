0b731c1756a377b65222b0993ebe07c447649541
Royalboe <auomidejohnson@gmail.com>
2022-09-24 06:08:32 +0100

    post emails

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""  script that takes in a URL and an email, sends a POST request to the
+    passed URL with the email as a parameter, and displays the body of the
+    response
+"""
+import requests
+from sys import argv
+
+
+def post_emailr():
+    """sends a POST request to the passed URL with the email as a parameter"""
+    url = argv[1]
+    values = {'email': argv[2]}
+    r = requests.post(url, data=values)
+    print(r.text)
+
+if __name__ == "__main__":
+    post_emailr()
