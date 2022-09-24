2ec1b7fec64e96be40998a02d7b4f86ad56d3a1c
Royalboe <auomidejohnson@gmail.com>
2022-09-24 06:04:59 +0100

    fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" script that fetches https://alx-intranet.hbtn.io/status """
+import requests
+
+
+def fetch_r():
+    """ fetches https://alx-intranet.hbtn.io/status """
+    req = requests.get('https://alx-intranet.hbtn.io/status')
+    s = "Body response:\n\t- type: {}\n\t- content: {}"
+    print(s.format(type(req.text), req.text))
+
+if __name__ == "__main__":
+    fetch_r()
