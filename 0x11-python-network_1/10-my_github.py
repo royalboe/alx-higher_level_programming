3d34acd28a3cae93c2d308c30edb50bf2ecd62e9
Royalboe <auomidejohnson@gmail.com>
2022-09-24 05:56:33 +0100

    script that takes github credentials to display id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""  script that takes your GitHub credentials (username and password) and
+    uses the GitHub API to display your id
+"""
+import requests
+from sys import argv
+
+
+def my_github():
+    """sends a POST request to the passed URL with a letter as a parameter"""
+    url = "https://api.github.com/user"
+    values = (argv[1], argv[2])
+    req = requests.get(url, auth=values)
+    if req.status_code >= 400:
+        print("None")
+    else:
+        print(req.json().get('id'))
+
+if __name__ == "__main__":
+    my_github()
