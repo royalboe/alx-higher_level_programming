f3f1638c24476c3f37a26251769258721315d984
Royalboe <auomidejohnson@gmail.com>
2022-09-24 06:02:31 +0100

    takes in url and displays response of a request

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""  script that takes in a URL, sends a request to the URL and
+    displays the body of the response (decoded in utf-8)
+"""
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
+from sys import argv
+
+
+def error_code():
+    """ sends a request to the URL and displays the body of the response """
+    url = argv[1]
+    try:
+        with urlopen(url) as response:
+            the_page = response.read()
+        print(the_page.decode('utf-8'))
+    except HTTPError as e:
+        print('Error code: {}'.format(e.code))
+if __name__ == "__main__":
+    error_code()
