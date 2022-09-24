5c799d480e273f12087ee22d170cad6928580eb6
Royalboe <auomidejohnson@gmail.com>
2022-09-24 06:06:56 +0100

    takes url, sends a request to the url

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""  script that takes in a URL, sends a request to the URL and displays
+    the value of the X-Request-Id variable found in the header of the response.
+"""
+import requests
+from sys import argv
+
+
+def request_idr():
+    """ displays the value of the X-Request-Id variable """
+    req = requests.get(argv[1])
+    print(req.headers.get('X-Request-Id'))
+
+if __name__ == "__main__":
+    request_idr()
