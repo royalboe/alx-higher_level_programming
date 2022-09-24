ce3952d244887e3e2f0d203e2b80060ac2786b4c
Royalboe <auomidejohnson@gmail.com>
2022-09-24 06:11:37 +0100

    takes a letter and sends a post request

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" script that takes in a letter and sends a POST request
+    to http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+import requests
+from sys import argv
+
+
+def json_api():
+    """ sends a POST request to the passed URL with a letter as a parameter """
+    url = "http://0.0.0.0:5000/search_user"
+    if len(argv) < 2:
+        values = {'q': ""}
+    else:
+        values = {'q': argv[1]}
+    r = requests.post(url, data=values)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print('[{}] {}'.format(response.get('id'), response.get('name')))
+    except:
+        print("Not a valid JSON")
+
+if __name__ == "__main__":
+    json_api()
