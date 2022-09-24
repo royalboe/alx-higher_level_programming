3f3789555823b4045016475a0d42684a9e1084aa
Royalboe <auomidejohnson@gmail.com>
2022-09-24 06:09:56 +0100

    takes in a url and sends request then displays body

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""  script that takes in a URL, sends a request to the URL and
+    displays the body of the response (decoded in utf-8)
+"""
+import requests
+from sys import argv
+
+
+def error_rcode():
+    """ sends a request to the URL and displays the body of the response """
+    response = requests.get(argv[1])
+    status = response.status_code
+    if status >= 400:
+        print('Error code: {}'.format(status))
+    else:
+        print(response.text)
+if __name__ == "__main__":
+    error_rcode()
