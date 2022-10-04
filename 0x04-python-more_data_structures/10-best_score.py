e444ec466d20cf4a6e53ff6d3f12baf6a3e6f880
Royalboe <auomidejohnson@gmail.com>
2022-10-04 20:28:37 +0100

    made adj

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,9 +3,9 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     biggest = 0
+    biggest_key = ""
     for key, value in a_dictionary.items():
         if value > biggest:
             biggest = value
-    for key, value in a_dictionary.items():
-        if value == biggest:
-            return key
+            biggest_key = key
+    return biggest_key
