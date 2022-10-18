041f8aa5cf92b55584a6b47f6ac73feeee8a677b
= <=>
2022-10-18 17:50:26 +0000

    conventions

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,8 +2,9 @@
 
 const request = require('request');
 
-let url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  console.log(JSON.parse(body).title);
 });
