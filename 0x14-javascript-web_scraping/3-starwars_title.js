e1a3828fb0555d13ad7cdb92f5c4823fbec63885
= <=>
2022-10-18 17:27:17 +0000

    fixed bug

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
-  if (error) {
-    console.error(error);
-  }
-  console.log(JSON.parse(body).title);
+let url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
 });
