49671c2223781c9a53200fb2cd541240bed9b8ca
Ayomide <auomidejohnson@gmail.com>
2022-10-26 12:36:29 -0700

    third task

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+// script that updates the text color of the HTML tag HEADER to red with jQuery API after clicking
+
+$('DIV#red_header').click(function () {
+    $('header').css({ color: '#FF0000' });
+});
