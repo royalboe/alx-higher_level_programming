2901e3c7904dd56ca7750184efd201c9bb6aa46e
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2023-05-05 20:57:47 +0100

    Update 7-main.py

--- 0x01-python-if_else_loops_functions/7-main.py
+++ 0x01-python-if_else_loops_functions/7-main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-print_last_digit = __import__('9-print_last_digit').print_last_digit
+islower = __import__('7-islower').islower
 
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
+print("a is {}".format("lower" if islower("a") else "upper"))
+print("H is {}".format("lower" if islower("H") else "upper"))
+print("A is {}".format("lower" if islower("A") else "upper"))
+print("3 is {}".format("lower" if islower("3") else "upper"))
+print("g is {}".format("lower" if islower("g") else "upper"))
