8c8aa6c7c369258bd17e3c962af30b1430a8ecc0
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2023-05-05 21:00:11 +0100

    Create 8-main.py

---
+++ 0x01-python-if_else_loops_functions/8-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+uppercase = __import__('8-uppercase').uppercase
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
+
+guillaume@ubuntu:~/0x01$ ./8-main.py
+BEST
+BEST SCHOOL 98 BATTERY STREET
