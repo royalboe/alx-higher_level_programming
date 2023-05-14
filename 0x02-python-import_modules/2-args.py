3c66c9f06630917ce27ec23bdffaafb426a804d9
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2023-05-14 16:39:32 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     import sys
 
     count = len(sys.argv) - 1
-    print("{} arguments.".format(count) if count == 0 else "1 argument:")
+    print("{} arguments.".format(count) if count != 1 else "1 argument:")
     for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
