4acfbad7b9713db86defa3156d48d3932301e02e
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2023-05-14 16:59:09 +0100

    Bug fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     elif i == 1:
         print("{} argument:".format(i))
     else:
-        print("{} arguments:".format(i)
+        print("{} arguments:".format(i))
 
     if i >= 1:
         i = 0
