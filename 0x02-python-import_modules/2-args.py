58bab867f1eb9c1d749e3659b4d8cdcc87ba1411
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2023-05-14 16:58:15 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,9 +8,7 @@ if __name__ == "__main__":
     elif i == 1:
         print("{} argument:".format(i))
     else:
-        print("{} arguments:".format(i))
-   
-# print("0 arguments." if i == 0 else "1 argument:" if i == 1 else "{} arguments:".format(i))
+        print("{} arguments:".format(i)
 
     if i >= 1:
         i = 0
