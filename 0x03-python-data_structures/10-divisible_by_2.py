6d527b811eacfeb8d7e76af7535e4df420fe6d54
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2023-05-21 19:40:21 +0100

    Update 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    return [True for x in my_list if x % 2 == 0 else False]
-    # return list(map(lambda x: True if x % 2 == 0 else False, my_list))
+    # return [True for x in my_list if x % 2 == 0 else False]
+    return list(map(lambda x: True if x % 2 == 0 else False, my_list))
