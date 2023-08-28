1e36151d203f2ba836902f1c849594cdfa12c763
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2023-08-28 08:10:04 +0100

    Added comments

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
+"""
+A program that prints all possible different combinations of two digits.
+
+Numbers are separated by ",", followed by a space
+The two digits are different
+01 and 10 are considered the same combination of the two digits 0 and 1
+Print only the smallest combination of two digits
+Numbers should be printed in ascending order, with two digits
+The last number should be followed by a new line
+You can only use no more than 3 print functions with string format
+You can only use no more than 2 loops in your code
+You are not allowed to store numbers or strings in a variable
+You are not allowed to import any module
+Running should give this output:
+01, 02, 03, 04, 05, 06, 07, 08, 09, 12, 13, 14, 15, 16, 17, 18, 19, 23, 24, 25, 26, 27, 28, 29, 34, 35, 36, 37, 38, 39, 45, 46, 47, 48, 49, 56, 57, 58, 59, 67, 68, 69, 78, 79, 89
+guillaume@ubuntu:~/0x01$ 
+"""
 for i in range(0, 9):
     for j in range(i + 1, 10):
         if i < 8:
--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
+    """
+    Function that checks for lowercase character.
+    """
     if ord(c) in range(97, 123):
         return True
     else:
